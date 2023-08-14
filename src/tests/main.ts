74a045f5dea9ad1b14f01463110992941ad8007c
Tronikelis <contactdonatas@gmail.com>
2023-08-14 17:55:48 +0300

    add initial test

--- src/tests/main.ts
+++ src/tests/main.ts
@@ -1,14 +1,22 @@
-import { expect, test } from "@jest/globals";
+import { expect, it } from "@jest/globals";
 import { renderHook } from "@solidjs/testing-library";
 
 import useSWR from "../lib";
 
-test("works", async () => {
+it("at least boots up", async () => {
     const { result } = renderHook(useSWR, [
         () => "https://jsonplaceholder.typicode.com/todos/1",
     ]);
 
-    await new Promise(r => setTimeout(r, 3e3));
+    expect(result.data()).toBe(undefined);
+    expect(result.error()).toBe(undefined);
+    expect(result.isLoading()).toBe(true);
+
+    while (result.data() === undefined) {
+        await new Promise(r => setTimeout(r, 100));
+    }
 
     expect(result.data()).not.toBe(undefined);
+    expect(result.error()).toBe(undefined);
+    expect(result.isLoading()).toBe(false);
 });
