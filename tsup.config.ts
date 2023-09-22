54a47b6793138e6b81733cf88b3b079ea5f8f726
Tronikelis <contactdonatas@gmail.com>
2023-09-22 17:52:39 +0300

    tsup preset solid

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,28 @@
+import * as preset from "tsup-preset-solid";
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-    clean: true,
-    format: ["cjs", "esm"],
-    entry: ["./src/lib/index.ts"],
-    dts: true,
+const presetOptions: preset.PresetOptions = {
+    entries: {
+        entry: "./src/lib/index.ts",
+    },
+    drop_console: true,
+};
+
+export default defineConfig(config => {
+    const watching = !!config.watch;
+
+    const parsedData = preset.parsePresetOptions(presetOptions, watching);
+
+    if (!watching) {
+        const packageFields = preset.generatePackageExports(parsedData);
+
+        console.log(`\npackage.json: \n${JSON.stringify(packageFields, null, 2)}\n\n`);
+
+        /*
+            will update ./package.json with the correct export fields
+        */
+        preset.writePackageJson(packageFields);
+    }
+
+    return preset.generateTsupOptions(parsedData);
 });
