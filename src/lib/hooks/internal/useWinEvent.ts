93a642140e57d5c407d53d24fb862faebcc8bd10
Tronikelis <contactdonatas@gmail.com>
2023-10-13 10:56:47 +0300

    use onMount for window events

--- src/lib/hooks/internal/useWinEvent.ts
+++ src/lib/hooks/internal/useWinEvent.ts
@@ -1,11 +1,13 @@
-import { onCleanup } from "solid-js";
+import { onCleanup, onMount } from "solid-js";
 
 type AnyFn = (...params: any[]) => void;
 
 export default function useWinEvent(type: string, cb: AnyFn) {
-    window.addEventListener(type, cb);
+    onMount(() => {
+        window.addEventListener(type, cb);
 
-    onCleanup(() => {
-        window.removeEventListener(type, cb);
+        onCleanup(() => {
+            window.removeEventListener(type, cb);
+        });
     });
 }
