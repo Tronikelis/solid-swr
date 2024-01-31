8971762217623b1a3bf3497786d8b476eacd2224
Tronikelis <contactdonatas@gmail.com>
2024-01-31 22:49:13 +0200

    remove extra reval local logic

--- src/tests/boundMutation.test.ts
+++ src/tests/boundMutation.test.ts
@@ -7,8 +7,6 @@ import createKey from "./utils/createKey";
 import waitForMs from "./utils/waitForMs";
 
 it("sets new value instantly and respects revalidation option", async () => {
-    let value = `${Math.random()}`;
-
     const fetcher = jest.fn(async (x: string) => {
         await waitForMs();
         return x;
@@ -19,46 +17,16 @@ it("sets new value instantly and respects revalidation option", async () => {
     const { result } = renderHook(useSWR, [key, { fetcher }]);
 
     await waitForMs();
-    expect(result.data()).not.toBe(value);
+    expect(result.data()).toBe(key());
     expect(fetcher).toBeCalledTimes(1);
 
-    void result.mutate(value, { revalidate: true });
-    expect(result.data()).toBe(value);
+    result.mutate("foo", { revalidate: true });
+    expect(result.data()).toBe("foo");
     expect(fetcher).toBeCalledTimes(2);
 
-    value = `${Math.random()}`;
-    expect(result.data()).not.toBe(value);
+    await waitForMs();
 
-    void result.mutate(value, { revalidate: false });
-    expect(result.data()).toBe(value);
+    result.mutate("bar", { revalidate: false });
+    expect(result.data()).toBe("bar");
     expect(fetcher).toBeCalledTimes(2);
-});
-
-it("returned.mutate() is bound to the hook", async () => {
-    const value = `${Math.random()}`;
-
-    const fetcher = jest.fn(async (x: string) => {
-        await waitForMs();
-        return x;
-    });
-
-    const [key] = createKey();
-
-    const { result } = renderHook(useSWR, [key, { fetcher }]);
-    const { result: resultOther } = renderHook(useSWR, [key, { fetcher }]);
-
-    await waitForMs();
-    expect(result.data()).not.toBe(value);
-    expect(resultOther.data()).not.toBe(value);
-
-    void result.mutate(value, { revalidate: false });
-    expect(result.data()).toBe(value);
-    expect(resultOther.data()).not.toBe(value);
-
-    // this await here is probably not needed
-    // but checking here also that if I accidentally sync up state using events
-    // let them finish before checking
-    await waitForMs();
-    expect(result.data()).toBe(value);
-    expect(resultOther.data()).not.toBe(value);
 });
