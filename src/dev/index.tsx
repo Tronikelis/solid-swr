473c8a9fc5c9e016deb5281ea050e456889ace8c
Tronikelis <contactdonatas@gmail.com>
2024-02-10 13:34:39 +0200

    dev folder is ignored

--- src/dev/index.tsx
+++ src/dev/index.tsx
@@ -1,17 +1,6 @@
 import { render } from "solid-js/web";
 
-import useSWR from "~/index";
-
-function useFetch() {
-    const swr = useSWR(() => `https://jsonplaceholder.typicode.com/todos/1`);
-    return swr;
-}
-
-function App() {
-    const { data } = useFetch();
-
-    return <>{JSON.stringify(data.v)}</>;
-}
+import App from "./App";
 
 const solidRoot = document.getElementById("solid-root") as HTMLDivElement;
 render(() => <App />, solidRoot);
