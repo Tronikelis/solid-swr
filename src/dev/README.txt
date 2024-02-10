2aef787e303515549adc0873d993a1666cf94aab
Tronikelis <contactdonatas@gmail.com>
2024-02-10 13:55:16 +0200

    remove dev dir

--- src/dev/README.txt
+++
@@ -1,1 +0,0 @@
-This dir is like a storybook just to quickly prototype some ideas or test some things for the useSWR hook
--- src/dev/index.tsx
+++
@@ -1,6 +0,0 @@
-import { render } from "solid-js/web";
-
-import App from "./App";
-
-const solidRoot = document.getElementById("solid-root") as HTMLDivElement;
-render(() => <App />, solidRoot);
